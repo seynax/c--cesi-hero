dd24b4da629ec6d0e0d0d566517d7d3ac698422d
Mateo-RENAC <mateo.renac@gmail.com>
2024-08-25 15:22:28 +0200

    Rework des models

--- Cesi--Hero/Cesi--Hero/Model/Power.cs
+++ Cesi--Hero/Cesi--Hero/Model/Power.cs
@@ -4,11 +4,17 @@
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public string Description { get; set; }
 
-        public Power(int id, string name)
+        // Navigation property for many-to-many relationship
+        public ICollection<Hero> Heroes { get; set; }
+
+        public Power(int id, string name,string description)
         {
             Id = id;
             Name = name;
+            Description = description;
+            Heroes = new List<Hero>();
         }
     }
 }
