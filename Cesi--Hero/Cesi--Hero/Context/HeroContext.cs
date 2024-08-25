cd9cfaea4e48cb2716ecf8d4991ab03b68133418
Mateo-RENAC <mateo.renac@gmail.com>
2024-08-25 15:35:01 +0200

    HeroContext Fix (Unable to access to the DB)

--- Cesi--Hero/Cesi--Hero/Context/HeroContext.cs
+++ Cesi--Hero/Cesi--Hero/Context/HeroContext.cs
@@ -5,20 +5,38 @@ namespace Cesi__Hero.Context
 {
     public class HeroContext : DbContext
     {
+        public DbSet<Hero> Heroes { get; set; }
+        public DbSet<Power> Powers { get; set; }
+        public DbSet<School> Schools { get; set; }
+
         public HeroContext()
         {
-
         }
+
+        // Database Config
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             base.OnConfiguring(optionsBuilder);
             optionsBuilder.UseSqlServer("Data Source=L35301222-10\\MSSQLSERVER01;Integrated Security=True;Encrypt=False");
         }
+
+        // Fluent API Config
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            // Relation One-to-Many between Hero and School
+            modelBuilder.Entity<Hero>()
+                .HasOne(h => h.School)
+                .WithMany(s => s.Heroes)
+                .HasForeignKey(h => h.SchoolId);
+
+            // Relation Many-to-Many between Hero and Power
+            modelBuilder.Entity<Hero>()
+                .HasMany(h => h.Powers)
+                .WithMany(p => p.Heroes)
+                .UsingEntity(j => j.ToTable("HeroPower"));
+
             base.OnModelCreating(modelBuilder);
         }
-
-        public DbSet<Hero> Heroes { get; set; }
     }
 }
+
